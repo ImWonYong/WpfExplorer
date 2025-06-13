a6e48ed47d3eb5f00c0e80a17fa8497cd83a0e99
ImWonYong <ssassb123@daum.net>
2025-06-13 15:41:32 +0900

    TreeView 작성

---
+++ WpfExplorer/WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Jamesnet.Wpf.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfExplorer.Support.Local.Helpers;
+using WpfExplorer.Support.Local.Models;
+
+namespace WpfExplorer.Main.Local.ViewModels
+{
+    public partial class MainContentViewModel : ObservableBase
+    {
+        private readonly FileService _fileService;
+
+        public List<FolderInfo> Roots { get; init; }
+
+        public MainContentViewModel(FileService fileService)
+        {
+            _fileService = fileService;
+
+            Roots = _fileService.GenerateRootNodes();
+        }
+
+        [RelayCommand]
+        private void FolderChanged(FolderInfo folderInfo)
+        {
+            _fileService.RefreshSubdirectories(folderInfo);
+        }
+    }
+}
